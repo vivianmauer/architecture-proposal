b576a301690ab73d0eed346e344731c25b2a7f4e
Vivian Mauer <vii.mauer@gmail.com>
2023-12-07 18:37:43 -0300

    add base url example

--- src/scripts/baseApi.ts
+++ src/scripts/baseApi.ts
@@ -2,6 +2,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const baseApi = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: '/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: '/project-base-url-here' }),
   endpoints: () => ({}),
 })
